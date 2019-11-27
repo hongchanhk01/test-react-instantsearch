6df9039f51c52f3179d769182303e0aaef3e8eac
hongchanhk01 <hongchan@hk01.com>
2019-11-27 14:47:25 +0800

    readme for getting start

--- NoResults.tsx
+++ NoResults.tsx
@@ -3,6 +3,9 @@ import { connectStateResults } from "react-instantsearch-dom"
 
 const NoResults = ({ allSearchResults }) => {
   console.log(allSearchResults)
+  if (allSearchResults) {
+    console.log("starships", allSearchResults.starships)
+  }
   return <h1>No Results</h1>
 }
 
